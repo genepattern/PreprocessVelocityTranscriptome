be51cfb2cccdb748958ddb0a5a30ae096ef9b947
Anthony S. Castanza <acastanza@ucsd.edu>
2021-11-06 13:15:17 -0700

    Update txome.for.velocity.R

--- src/txome.for.velocity.R
+++ src/txome.for.velocity.R
@@ -1,47 +1,43 @@
-#Prepare.Transcriptome.for.Velocity
+# Prepare.Transcriptome.for.Velocity
 # Source: https://combine-lab.github.io/alevin-tutorial/2020/alevin-velocity/
 
 suppressPackageStartupMessages({
-    library("devtools")
-    library("getopt")
-    library("optparse")
-    library("R.utils")
-    library("dplyr")
-    library("BSgenome")
-    library("eisaR")
-    library("GenomicFeatures")
-#    library(SummarizedExperiment)
-#    library(tximeta)
-#    library(rjson)
-#    library(reticulate)
-#    library(SingleCellExperiment)
-#    library(scater)
-    library("Biostrings")
+  library("getopt")
+  library("optparse")
+  library("R.utils")
+  library("dplyr")
+  library("BSgenome")
+  library("eisaR")
+  library("GenomicFeatures")
+  library("Biostrings")
 })
 
-arguments <- commandArgs(trailingOnly=TRUE)
+arguments <- commandArgs(trailingOnly = TRUE)
 
 option_list <- list(
-make_option("--gtf", dest = "gtf"),
-make_option("--genome", dest = "genome.fa"),
-make_option("--type", dest = "type"),
-make_option("--length", dest = "length"))
+  make_option("--gtf", dest = "gtf"),
+  make_option("--genome", dest = "genome.fa"),
+  make_option("--type", dest = "type"),
+  make_option("--length", dest = "length")
+)
 
-opt <- parse_args(OptionParser(option_list = option_list), positional_arguments = TRUE,
- args = arguments)$options
+opt <- parse_args(OptionParser(option_list = option_list),
+  positional_arguments = TRUE,
+  args = arguments
+)$options
 
-gtf = opt$gtf
-genome.fa = opt$genome.fa
-type = opt$type ## "separate", or "collapse"
-length = opt$length ## Insert Length - 1 (default 90)
+gtf <- opt$gtf
+genome.fa <- opt$genome.fa
+type <- opt$type ## "separate", or "collapse"
+length <- opt$length ## Insert Length - 1 (default 90)
 
-#genome.fa.out <- gsub(basename(genome.fa), pattern=".gz$", replacement="")
-#genome.fa.out <- gsub(genome.fa.out, pattern=".fa$", replacement="")
-#genome.fa.out <- paste0(genome.fa.out,".expanded")
+# genome.fa.out <- gsub(basename(genome.fa), pattern=".gz$", replacement="")
+# genome.fa.out <- gsub(genome.fa.out, pattern=".fa$", replacement="")
+# genome.fa.out <- paste0(genome.fa.out,".expanded")
 
-sequences.out <- gsub(basename(gtf), pattern=".gz$", replacement="")
-sequences.out <- gsub(sequences.out, pattern=".gtf$", replacement="")
-sequences.out <- paste0(sequences.out,".velocity")
+sequences.out <- gsub(basename(gtf), pattern = ".gz$", replacement = "")
+sequences.out <- gsub(sequences.out, pattern = ".gtf$", replacement = "")
+sequences.out <- paste0(sequences.out, ".velocity")
 
 grl <- eisaR::getFeatureRanges(
   gtf = gtf,
@@ -53,12 +49,12 @@ grl <- eisaR::getFeatureRanges(
 )
 
 genome <- Biostrings::readDNAStringSet(
-    genome.fa
-) #The "Error in (function (x)  : attempt to apply non-function" message is expected.
+  genome.fa
+) # The "Error in (function (x)  : attempt to apply non-function" message is expected.
 
 gtf_df <- rtracklayer::import(gtf)
-gtf_df=as.data.frame(gtf_df)
-gene_meta = distinct(gtf_df[,c("gene_id","gene_name")])
+gtf_df <- as.data.frame(gtf_df)
+gene_meta <- distinct(gtf_df[, c("gene_id", "gene_name")])
 
 names(genome) <- sapply(strsplit(names(genome), " "), .subset, 1)
 seqs <- GenomicFeatures::extractTranscriptSeqs(
@@ -67,39 +63,41 @@ seqs <- GenomicFeatures::extractTranscriptSeqs(
 )
 
 Biostrings::writeXStringSet(
-    seqs, filepath = paste0(sequences.out,".",as.character(length),"bp_flank.fa.gz"), compress=TRUE
+  seqs,
+  filepath = paste0(sequences.out, ".", as.character(length), "bp_flank.fa.gz"), compress = TRUE
 )
 
 eisaR::exportToGtf(
   grl,
-  filepath = paste0(sequences.out,".gtf")
+  filepath = paste0(sequences.out, ".gtf")
 )
-gzip(paste0(sequences.out,".gtf"), destname=paste0(sequences.out,".",as.character(length),"bp_flank.gtf.gz"), overwrite=TRUE, remove=TRUE)
+gzip(paste0(sequences.out, ".gtf"), destname = paste0(sequences.out, ".", as.character(length), "bp_flank.gtf.gz"), overwrite = TRUE, remove = TRUE)
 
-splicing_meta = metadata(grl)$corrgene
-metadata_table = merge(x=splicing_meta, y=gene_meta, by.x="spliced", by.y="gene_id", all.x=TRUE)
-metadata_table = metadata_table[match(splicing_meta$spliced,metadata_table$spliced),]
+splicing_meta <- metadata(grl)$corrgene
+metadata_table <- merge(x = splicing_meta, y = gene_meta, by.x = "spliced", by.y = "gene_id", all.x = TRUE)
+metadata_table <- metadata_table[match(splicing_meta$spliced, metadata_table$spliced), ]
 
 write.table(metadata_table,
-    file = paste0(sequences.out,".",as.character(length),"bp_flank.features.tsv"),
-    row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t"
+  file = paste0(sequences.out, ".", as.character(length), "bp_flank.features.tsv"),
+  row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t"
 )
 
 df <- eisaR::getTx2Gene(
-    grl, filepath = paste0(sequences.out,".",as.character(length),"bp_flank.tgMap.tsv")
+  grl,
+  filepath = paste0(sequences.out, ".", as.character(length), "bp_flank.tgMap.tsv")
 )
 
-mt_df=unique(gtf_df[(gtf_df$seqnames=="chrM"|gtf_df$seqnames=="MT"),c("gene_id"),drop=FALSE])
-mt_df_2=as.data.frame(paste(mt_df$gene_id,"I", sep="-"))
-colnames(mt_df_2)=colnames(mt_df)
-mt_df=rbind(mt_df,mt_df_2)
-write.table(mt_df,paste0(sequences.out,".",as.character(length),"bp_flank.mtGenes.txt"),quote=FALSE,row.names=FALSE,col.names=FALSE,sep="\t")
+mt_df <- unique(gtf_df[(gtf_df$seqnames == "chrM" | gtf_df$seqnames == "MT"), c("gene_id"), drop = FALSE])
+mt_df_2 <- as.data.frame(paste(mt_df$gene_id, "I", sep = "-"))
+colnames(mt_df_2) <- colnames(mt_df)
+mt_df <- rbind(mt_df, mt_df_2)
+write.table(mt_df, paste0(sequences.out, ".", as.character(length), "bp_flank.mtGenes.txt"), quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
 
-rrna_df=unique(gtf_df[gtf_df$gene_type=="rRNA",c("gene_id"),drop=FALSE])
-rrna_df_2=as.data.frame(paste(rrna_df$gene_id,"I", sep="-"))
-colnames(rrna_df_2)=colnames(rrna_df)
-rrna_df=rbind(rrna_df,rrna_df_2)
-write.table(rrna_df,paste0(sequences.out,".",as.character(length),"bp_flank.rrnaGenes.txt"),quote=FALSE,row.names=FALSE,col.names=FALSE,sep="\t")
+rrna_df <- unique(gtf_df[gtf_df$gene_type == "rRNA", c("gene_id"), drop = FALSE])
+rrna_df_2 <- as.data.frame(paste(rrna_df$gene_id, "I", sep = "-"))
+colnames(rrna_df_2) <- colnames(rrna_df)
+rrna_df <- rbind(rrna_df, rrna_df_2)
+write.table(rrna_df, paste0(sequences.out, ".", as.character(length), "bp_flank.rrnaGenes.txt"), quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
 
 
 invisible(file.remove(gtf))
