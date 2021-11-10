ac53388cc1332636618aa6ab1663ee8f907c49dd
Anthony S. Castanza <acastanza@ucsd.edu>
2021-11-10 12:36:41 -0800

    Implement advanced options

--- src/txome.for.velocity.R
+++ src/txome.for.velocity.R
@@ -17,8 +17,10 @@ arguments <- commandArgs(trailingOnly = TRUE)
 option_list <- list(
   make_option("--gtf", dest = "gtf"),
   make_option("--genome", dest = "genome.fa"),
+  make_option("--length", dest = "length"),
+  make_option("--trim", dest = "trim"),
   make_option("--type", dest = "type"),
-  make_option("--length", dest = "length")
+  make_option("--join", dest = "join")
 )
 
 opt <- parse_args(OptionParser(option_list = option_list),
@@ -30,8 +32,9 @@ gtf = opt$gtf
 genome.fa = opt$genome.fa
 type = opt$type ## "separate", or "collapse"
 read_length = as.integer(opt$length) ## Insert Length, used to set flank length around introns
-flank_trim_length = 1 # New tutorial uses 5, not clear why.
+flank_trim_length = as.integer(opt$trim) # New tutorial uses 5, not clear why.
 flank_length = as.integer(read_length - flank_trim_length)
+join_introns = as.logical(opt$join)
 
 # genome.fa.out <- gsub(basename(genome.fa), pattern=".gz$", replacement="")
 # genome.fa.out <- gsub(genome.fa.out, pattern=".fa$", replacement="")
@@ -46,7 +49,7 @@ grl <- eisaR::getFeatureRanges(
   featureType = c("spliced", "intron"),
   intronType = type,
   flankLength = flank_length,
-  joinOverlappingIntrons = FALSE, # New tutorial sets = TRUE, not clear why
+  joinOverlappingIntrons = join_introns, # New tutorial sets = TRUE, not clear why
   verbose = FALSE
 )
 
@@ -66,34 +69,34 @@ seqs <- GenomicFeatures::extractTranscriptSeqs(
 
 Biostrings::writeXStringSet(
   seqs,
-  filepath = paste0(sequences.out, ".", as.character(length), "bp_flank.fa.gz"), compress = TRUE
+  filepath = paste0(sequences.out, ".", as.character(flank_length), "bp_flank.fa.gz"), compress = TRUE
 )
 
 eisaR::exportToGtf(
   grl,
   filepath = paste0(sequences.out, ".gtf")
 )
-gzip(paste0(sequences.out, ".gtf"), destname = paste0(sequences.out, ".", as.character(length), "bp_flank.gtf.gz"), overwrite = TRUE, remove = TRUE)
+gzip(paste0(sequences.out, ".gtf"), destname = paste0(sequences.out, ".", as.character(flank_length), "bp_flank.gtf.gz"), overwrite = TRUE, remove = TRUE)
 
 splicing_meta <- metadata(grl)$corrgene
 metadata_table <- merge(x = splicing_meta, y = gene_meta, by.x = "spliced", by.y = "gene_id", all.x = TRUE)
 metadata_table <- metadata_table[match(splicing_meta$spliced, metadata_table$spliced), ]
 
 write.table(metadata_table,
-  file = paste0(sequences.out, ".", as.character(length), "bp_flank.features.tsv"),
+  file = paste0(sequences.out, ".", as.character(flank_length), "bp_flank.features.tsv"),
   row.names = FALSE, col.names = TRUE, quote = FALSE, sep = "\t"
 )
 
 df <- eisaR::getTx2Gene(
   grl,
-  filepath = paste0(sequences.out, ".", as.character(length), "bp_flank.tgMap.tsv")
+  filepath = paste0(sequences.out, ".", as.character(flank_length), "bp_flank.tgMap.tsv")
 )
 
 mt_df <- unique(gtf_df[(gtf_df$seqnames == "chrM" | gtf_df$seqnames == "MT"), c("gene_id"), drop = FALSE])
 mt_df_2 <- as.data.frame(paste(mt_df$gene_id, "I", sep = "-"))
 colnames(mt_df_2) <- colnames(mt_df)
 mt_df <- rbind(mt_df, mt_df_2)
-write.table(mt_df, paste0(sequences.out, ".", as.character(length), "bp_flank.mtGenes.txt"), quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
+write.table(mt_df, paste0(sequences.out, ".", as.character(flank_length), "bp_flank.mtGenes.txt"), quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
 
 rrna_df <- unique(gtf_df[gtf_df$gene_type == "rRNA", c("gene_id"), drop = FALSE])
 rrna_df_2 <- as.data.frame(paste(rrna_df$gene_id, "I", sep = "-"))
